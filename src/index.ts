d35906e2509444a119b4fee23042f04795ea5a8f
mdy6099 <mdy6099@naver.com>
2019-09-07 21:39:18 +0900

    finished blockchain 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CrypytoJS from "crypto-js"
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ):string =>
+        CrypytoJS.SHA256(index + previousHash + data + timestamp.toString()) 
+
     constructor(
         index: number,
         hash: string,
@@ -22,6 +33,12 @@ class Block {
 const genesisBlock:Block = new Block(0, "2020200202020", "", "Hello", 123456)
 
 let blockChain: [Block] = [genesisBlock]
+
+const getBlockchain = ():Block[] => blockChain;
+
+const getLatestBlock = ():Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = ():number => Math.round(new Date().getTime()/1000);
 
 console.log(blockChain);
 
