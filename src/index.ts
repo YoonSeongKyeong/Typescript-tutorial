2f99bf1bfe47d968bd8bb33a0bac4ac224015969
mdy6099 <mdy6099@naver.com>
2019-09-07 21:22:51 +0900

    finished class 1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-interface Human {
-    name:string;
-    age:number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name:string, age:number, gender:string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
-
 
 const person = {
     name: "YoonseongKyeong",
@@ -11,7 +15,7 @@ const person = {
     gender: "male"
 }
 
-const sayHi = (person:Human): string => {
+const sayHi = (person: Human): string => {
     return (`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 }
 
